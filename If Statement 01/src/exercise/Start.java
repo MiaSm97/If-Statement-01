7dda015eaea94e21c32bd1a47e40ececed8ec5c4
MiaSm97 <118892985+MiaSm97@users.noreply.github.com>
2022-12-21 09:46:04 +0100

    Questo dovrebbe essere l'esercizio giusto

---
+++ If Statement 01/src/exercise/Start.java
@@ -0,0 +1,14 @@
+package exercise;
+
+public class Start {
+    public static void main(String[] args) {
+        String myName = "Mia";
+        if (myName.length() % 2 == 0) {
+            System.out.println("The letters of my name are even");
+        } else {
+            System.out.println("The letters of my name are odd");
+        }
+    }
+}
+    /*define a variable called myName and assign your name to it
+        using the if-else statement, print if the number of letters in your name is even or odd*/
